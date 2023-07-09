c3c0eefc72b274bbb01f90f5478154de18e89a90
Raul <raul.alhena@gmail.com>
2023-07-09 21:49:58 +0200

    boxed and space between

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Toolbar, AppBar, Container } from '@mui/material'
+import { Toolbar, AppBar, Container, Typography, Box } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -6,17 +6,23 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
         <AppBar component='nav' sx={{ border: '1px solid red', bgcolor: 'white'}}>
-            <Container >
-                <Toolbar>
-                    <Link to='/'>
-                        <img src="../../../assets/logo.png" alt="Soundwave" />Soundwave
-                    </Link>
-                    <Link to='/discover'>
-                        Discover
-                    </Link>
-                    <Link to='/join'>
-                        Join
-                    </Link>
+            <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0px'}}>
+                <Toolbar sx={{ border: '1px solid blue', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0px'}}>
+                    <Box>
+                        <Link to='/' style={{ border: '1px solid orange', alignItems: 'center' }}>
+                            <Typography variant='h5'>
+                                <img src="../../../assets/logo.png" alt="Soundwave" />Soundwave
+                            </Typography>
+                        </Link>
+                    </Box>
+                    <Box>
+                        <Link to='/discover'>
+                            Discover
+                        </Link>
+                        <Link to='/join'>
+                            Join
+                        </Link>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
