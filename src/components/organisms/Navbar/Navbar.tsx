9e1511e3da01cebd14829d8589fbe88ed8ec0734
Raul <raul.alhena@gmail.com>
2023-07-09 20:09:48 +0200

    material ui

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,18 +1,21 @@
+import { Container } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <nav>
-        <li>
-            <Link to='/'>Home</Link>
-        </li>
-        <li>
-            <Link to='/discover'>Discover</Link>
-        </li>
-        <li>
-            <Link to='/join'>Join</Link>
-        </li>
-    </nav>
+    <Container>
+         <nav>
+            <li>
+                <Link to='/'><img src="../../../assets/logo.png" alt="Soundwave" /></Link>
+            </li>
+            <li>
+                <Link to='/discover'>Discover</Link>
+            </li>
+            <li>
+                <Link to='/join'>Join</Link>
+            </li>
+        </nav>
+    </Container>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <CssBaseline>
       <App />
+    </CssBaseline>
     </BrowserRouter>
   </React.StrictMode>,
 )
