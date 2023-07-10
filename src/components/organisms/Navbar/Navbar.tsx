0d9b2f911ee2784c39da3a0e7ce7c28b44b33c9a
Raul <raul.alhena@gmail.com>
2023-07-10 11:07:23 +0200

    navbar size and position done

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,28 +1,60 @@
 import { Toolbar, AppBar, Container, Typography, Box } from '@mui/material'
+import { styled } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { makeStykes } from '@mui/material/core'
+
+
+const AppBarSx = {
+    border: '1px solid purple',
+    bgcolor: 'white',
+    display: 'flex'
+}
+
+const containerSx = {
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    padding: '0px',
+    width: '100%'
+}
+
+const MenuLinks = styled(Box)(() => ({
+    fontSize: '25px',
+    display: 'flex',
+    gap: '10px'
+}))
+
+const LogoTypography = styled(Typography)(() => ({
+    display: 'inline-flex',
+    gap: '10px',
+    fontSize:'30px',
+    border: '1px solid orange', 
+    alignItems: 'center', 
+    width: '100%' 
+}))
 
 
 export const Navbar = () => {
   return (
-        <AppBar component='nav' sx={{ border: '1px solid red', bgcolor: 'white'}}>
-            <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0px'}}>
+        <AppBar  component='nav' sx={AppBarSx}>
+            <Container sx={containerSx}>
                 <Toolbar sx={{ border: '1px solid blue', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0px'}}>
                     <Box>
-                        <Link to='/' style={{ border: '1px solid orange', alignItems: 'center' }}>
-                            <Typography variant='h5'>
+                        <Link to='/'>
+                            <LogoTypography variant='h5'>
                                 <img src="../../../assets/logo.png" alt="Soundwave" />Soundwave
-                            </Typography>
+                            </LogoTypography>
                         </Link>
                     </Box>
-                    <Box>
+                    <MenuLinks>
                         <Link to='/discover'>
                             Discover
                         </Link>
                         <Link to='/join'>
                             Join
                         </Link>
-                    </Box>
+                    </MenuLinks>
                 </Toolbar>
             </Container>
         </AppBar>
